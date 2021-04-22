fcb362505bc4a4434071473a5f7f40eb48356d55
Pablo Cavalcante Nunes <pabloo.cavalcante@gmail.com>
2021-04-22 08:52:46 -0300

    Primeiro commit do VsCode

---
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        Text("Questão 04"),
+        Text("de 10"),
+      ]),
+    );
+  }
+}
