da2c0426e6e474098d620bf302e69402d5a77ee2
pablo.nunes <pablo.nunes@totvs.com.br>
2021-04-22 18:10:18 -0300

    NLW#05 - Aula 02

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -37,12 +38,7 @@ class QuizCardWidget extends StatelessWidget {
             ),
             Expanded(
               flex: 3,
-              child: LinearProgressIndicator(
-                value: 0.3,
-                backgroundColor: AppColors.chartSecondary,
-                valueColor:
-                    AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-              ),
+              child: ProgressIndicatorWidget(value: 0.3,),
             )
           ],
         )
--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,4 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(AppWidget());
-  //runApp(AppWidget());
 }
